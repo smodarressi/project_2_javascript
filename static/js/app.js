b5eaf8abc8ccd3eaa34f9dc3a5c745c9e780b8ba
Decker <45881871+DeckerAN@users.noreply.github.com>
2019-06-25 19:49:42 -0700

    as done as we be

--- static/js/app.js
+++ static/js/app.js
@@ -14,7 +14,7 @@ function builddata() {
           console.log(selectValue1);
       var inputElement1 = d3.select("#inputWinery");
       var inputValue1 = inputElement1.property("value");
-          console.log(inputValue1);    
+          console.log(inputValue1);
 
       if (selectValue !== 'Choose...' && inputValue1 !== null && selectValue1 !== 'Choose...') {
         var filteredData = url.filter(url => url.businesstype === selectValue && url.names.includes(inputValue1) && url.city === selectValue1)}
@@ -26,9 +26,13 @@ function builddata() {
         var filteredData = url.filter(url => url.businesstype === selectValue && url.city === selectValue1)}
       else if (selectValue === 'Choose...' && inputValue1 !== null && selectValue1 === 'Choose...') {
         var filteredData = url.filter(url => url.names.includes(inputValue1))};
-
+      
       console.log(filteredData);
-    
+      
+      /// MAP AREA
+      marker_maker(filteredData);
+      /// END MAP AREA  
+
       d3.select("tbody").text("");
 
       filteredData.forEach((entry) => {
@@ -43,8 +47,60 @@ function builddata() {
   });
 }
 
+
+/// MAP AREA
+var mymap = L.map('sherwins-map').setView([37.524420, -122.265870], 5);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: API_KEY
+}).addTo(mymap);
+
+
+
+//idealy used after the submit button passes the information
+function marker_maker(json_filtered){
+  // to be changed to forEach once it is added to the filter
+  json_filtered.forEach((data) => {
+    // console.log(json_filtered[i]);
+    console.log(data.businesstype);
+    console.log(data.names);
+    var marker_icon;
+    if (data.businesstype === "Breweries"){
+      marker_icon = {
+        icon: L.AwesomeMarkers.icon({icon: 'beer', prefix: 'fa', markerColor: 'blue'})
+      }
+    }else if (data.businesstype === "Wineries"){
+      marker_icon = {
+        icon: L.AwesomeMarkers.icon({icon: 'glass-martini', prefix: 'fa', markerColor: 'darkred'})
+      }
+    }else{
+      marker_icon = {
+        icon : null
+      }
+    }
+    // console.log(json_filtered[i]);
+    console.log(data.long);
+    // makes markers based on filtered data marker
+    L.marker([data.lat,data.long], marker_icon).addTo(mymap)
+      .bindPopup("<strong>Biz Name: </strong>" + data.names + "<br><strong>Address: </strong>" + data.address + "<br><strong>Zip: </strong>" 
+      + data.zip + "<br><strong>Yelp Rating: </strong>" + data.yelprating + "<strong><br>Number of Yelp Reviews: </strong>" + data.numberofyelpreviews);
+
+});
+}
+
+/// END MAP AREA
+
+
  
 function init() {
+
+  /// MAP AREA
+
+  /// END MAP AREA
+
   // Grab a reference to the dropdown select element in city field
   var selector = d3.select("#inputCity");
   var url1 = `/city`;
---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoic21vZGFycmVzc2kiLCJhIjoiY2p3bGU3anZpMDBubDQ4b2M1a3RpY2Y0OCJ9.upUfp4PcfpFO35-E2HLTRQ";
